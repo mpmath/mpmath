b6b6aa5b8bdbba4cb29749e4f6ffc2e5fdafc4ae
Fredrik Johansson <fredrik.johansson@gmail.com>
2007-11-24 13:03:57 +0000

    update version to 0.5 in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(name='mpmath',
       description = 'Python library for arbitrary-precision floating-point arithmetic',
-      version='0.4',
+      version='0.5',
       url='http://mpmath.googlecode.com',
       author='Fredrik Johansson',
       author_email='fredrik.johansson@gmail.com',
