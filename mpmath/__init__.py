3e93e0539f7dedda6d0bd17cb309c769efb976af
Fredrik Johansson <fredrik.johansson@gmail.com>
2007-12-05 14:21:59 +0000

    in-progress interval arithmetic module

--- mpmath/__init__.py
+++ mpmath/__init__.py
@@ -2,3 +2,4 @@ import lib
 from mpmath import *
 from functions2 import gamma, factorial, lower_gamma, upper_gamma, erf, zeta
 from quad import quadts
+from interval import mpi
---
+++ mpmath/interval.py
@@ -0,0 +1,124 @@
+from mpmath import *
+
+class mpi:
+
+    def __init__(self, a, b=None):
+        if isinstance(a, mpi):
+            self.a = a.a
+            self.b = a.b
+            return
+        if b is None:
+            b = a
+        mpf.round_down()
+        self.a = mpf(a)
+        mpf.round_up()
+        self.b = mpf(b)
+        mpf.round_default()
+        assert self.a <= self.b, "endpoints must be properly ordered"
+
+    def __repr__(self):
+        mpf.dps += 5
+        s = "[%s, %s]"% (str(self.a), str(self.b))
+        mpf.dps -= 5
+        return s
+
+    def __contains__(self, x):
+        """Return True if x is contained in the interval, otherwise False."""
+        return (self.a <= x) and (x <= self.b)
+
+    def __neg__(self):
+        return 0 - self
+
+    def __add__(s, t):
+        t = mpi(t)
+        mpf.round_down()
+        a = s.a + t.a
+        mpf.round_up()
+        b = s.b + t.b
+        mpf.round_default()
+        return mpi(a, b)
+
+    __radd__ = __add__
+
+    def __sub__(s, t):
+        t = mpi(t)
+        mpf.round_down()
+        a = s.a - t.b
+        mpf.round_up()
+        b = s.b - t.a
+        mpf.round_default()
+        return mpi(a, b)
+
+    def __rsub__(s, t):
+        return mpi(t) - s
+
+    def __mul__(s, t):
+        t = mpi(t)
+        mpf.round_down()
+        xd, yd, zd, wd = s.a*t.a, s.a*t.b, s.b*t.a, s.b*t.b
+        a = min(xd,yd,zd,wd)
+        mpf.round_up()
+        xu, yu, zu, wu = s.a*t.a, s.a*t.b, s.b*t.a, s.b*t.b
+        b = max(xu,yu,zu,wu)
+        mpf.round_default()
+        return mpi(a, b)
+
+    __rmul__ = __mul__
+
+    def __div__(s, t):
+        t = mpi(t)
+        if 0 in t:
+            raise ZeroDivisionError, "cannot divide by interval containing 0"
+        mpf.round_down()
+        xd, yd, zd, wd = s.a/t.a, s.a/t.b, s.b/t.a, s.b/t.b
+        a = min(xd,yd,zd,wd)
+        mpf.round_up()
+        xu, yu, zu, wu = s.a/t.a, s.a/t.b, s.b/t.a, s.b/t.b
+        b = max(xu,yu,zu,wu)
+        mpf.round_default()
+        return mpi(a, b)
+
+    def __rdiv__(s, t):
+        return mpi(t) / s
+
+    __truediv__ = __div__
+    __rtruediv__ = __rdiv__
+    __floordiv__ = __div__
+    __rfloordiv__ = __rdiv__
+
+    def __pow__(s, t):
+        if not isinstance(t, mpi) and 0 < s.a:
+            n = int(t)
+            if n == t:
+                mpf.round_down()
+                a = s.a ** n
+                b = s.b ** n
+                mpf.round_up()
+                c = s.a ** n
+                d = s.b ** n
+                mpf.round_default()
+                aa = min(a,b,c,d)
+                bb = max(a,b,c,d)
+                return mpi(aa, bb)
+        t = mpi(t)
+        if 0 < s.a <= 1 and 0 < s.b <= 1:
+            return 1 / (s ** (-t))
+        else:
+            assert s.a >= 1 and s.b >= 1
+        assert s.a >= 0 and s.b >= 0
+        mpf.round_down()
+        a = exp(t.a*log(s.a))
+        mpf.round_up()
+        b = exp(t.b*log(s.b))
+        mpf.round_default()
+        return mpi(a, b)
+
+"""
+from mpmath import *
+mpi(1, 2)**2
+mpi(1) + mpi(0, 1e-50)
+mpi(2) ** mpi(2)
+mpi(2) ** mpi(0.5)
+mpi(2) + mpi(3,4)
+
+"""
