f883dc236ee4109b2598281c68bd9f8058caac25
Fredrik Johansson <fredrik.johansson@gmail.com>
2008-01-13 15:29:05 +0000

    preparing for release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(name='mpmath',
       description = 'Python library for arbitrary-precision floating-point arithmetic',
-      version='0.5',
+      version='0.6',
       url='http://mpmath.googlecode.com',
       author='Fredrik Johansson',
       author_email='fredrik.johansson@gmail.com',
