b08efc62c674578d792dc2c2370d4a42d8498820
Fredrik Johansson <fredrik.johansson@gmail.com>
2008-01-13 11:51:34 +0000

    update project files

--- setup.py
+++ setup.py
@@ -7,6 +7,6 @@ setup(name='mpmath',
       author='Fredrik Johansson',
       author_email='fredrik.johansson@gmail.com',
       license = 'BSD',
-      packages=['mpmath', 'mpmath/lib'],
+      packages=['mpmath', 'mpmath/lib', 'mpmath/apps', 'mpmath/tests'],
       classifiers=['Topic :: Scientific/Engineering :: Mathematics']
      )
