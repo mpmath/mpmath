9c6a8ac2fe7609686e50d3d91bb7707fefd42998
Fredrik Johansson <fredrik.johansson@gmail.com>
2008-04-19 15:26:53 +0000

    updates to documentation and setup files, in anticipation of 0.8 release

--- doc/build.py
+++ doc/build.py
@@ -1,2 +1,4 @@
 import os
+if not os.path.exists("build"):
+    os.mkdir("build")
 os.system("sphinx-build source build")
--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from distutils.core import setup
 
 setup(name='mpmath',
       description = 'Python library for arbitrary-precision floating-point arithmetic',
-      version='0.7',
+      version='0.8',
       url='http://mpmath.googlecode.com',
       author='Fredrik Johansson',
       author_email='fredrik.johansson@gmail.com',
       license = 'BSD',
-      packages=['mpmath', 'demo', 'mpmath/tests'],
+      packages=['mpmath', 'mpmath/tests'],
       classifiers=['Topic :: Scientific/Engineering :: Mathematics']
      )
