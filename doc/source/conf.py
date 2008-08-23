263724643a0f2df2f2e5def022b993d3ab9554b3
Fredrik Johansson <fredrik.johansson@gmail.com>
2008-08-23 20:42:05 +0000

    fix docs to say version 0.9 instead of 0.8

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -40,9 +40,9 @@ copyright = '2008, Fredrik Johansson'
 # other places throughout the built documents.
 #
 # The short X.Y version.
-version = '0.8'
+version = '0.9'
 # The full version, including alpha/beta/rc tags.
-release = '0.8'
+release = '0.9'
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
