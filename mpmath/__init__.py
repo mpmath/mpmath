d6dbfc2bbc24402ea1a3223fe0b21905b287955c
Fredrik Johansson <fredrik.johansson@gmail.com>
2008-10-04 16:07:31 +0000

    move pslq and related functions to a standalone file

--- mpmath/__init__.py
+++ mpmath/__init__.py
@@ -1,21 +1,20 @@
 __version__ = '0.9'
 
 from mptypes import *
+
 from functions import *
+from elliptic import jacobi_theta_1, jacobi_theta_2, jacobi_theta_3, \
+    jacobi_theta_4, jacobi_elliptic_sn, jacobi_elliptic_cn, jacobi_elliptic_dn
 
 from calculus import diff, diffc, secant, polyval, polyroots, quadosc
 from calculus import sumem, chebyfit, sumsh, sumrich, limit
 from calculus import odeint
-from calculus import pslq, identify, findpoly
-
 from quadrature import quad, quadgl, quadts, TanhSinh, GaussLegendre
 
-from elliptic import jacobi_theta_1, jacobi_theta_2, jacobi_theta_3, \
-    jacobi_theta_4, jacobi_elliptic_sn, jacobi_elliptic_cn, jacobi_elliptic_dn
+from identification import pslq, identify, findpoly
 
 from matrices import matrix, eye, diag, zeros, ones, randmatrix, mnorm_1, \
     mnorm_oo, mnorm_F, norm_p
-
 from linalg import lu_solve, inverse, residual, qr_solve, cholesky_solve, det, \
     cond
 
