90914bbad19d789ffb903249b071e68082008e54
Fredrik Johansson <fredrik.johansson@gmail.com>
2008-10-14 15:24:59 +0000

    fix a bug in matrix.__repr__ for odd types

--- mpmath/matrices.py
+++ mpmath/matrices.py
@@ -115,6 +115,8 @@ class matrix(object):
                     a = repr(self[i,j])
                 else:  #TODO: for mpc too
                     a = "'" + str(self[i,j]) + "'"
+                else:
+                    a = repr(self[i,j])
                 s += a + ', '
             s = s[:-2]
             s += '],\n '
