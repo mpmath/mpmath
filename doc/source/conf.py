75f33471ca103c46fb58d6dd30afa19d0edb2f5d
Fredrik Johansson <fredrik.johansson@gmail.com>
2008-10-14 16:37:55 +0000

    more doc updates

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -40,9 +40,10 @@ copyright = '2008, Fredrik Johansson'
 # other places throughout the built documents.
 #
 # The short X.Y version.
-version = '0.9'
+import mpmath
+version = mpmath.__version__
 # The full version, including alpha/beta/rc tags.
-release = '0.9'
+release = mpmath.__version__
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
