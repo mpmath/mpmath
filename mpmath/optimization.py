2ee7f3bb761f379a6afadede4e00ce4f0c071648
Vinzent Steinberg <vinzent.steinberg@gmail.com>
2008-10-21 09:39:26 +0000

    copy examples from optimization docs to corresponding docstrings

--- mpmath/optimization.py
+++ mpmath/optimization.py
@@ -501,6 +501,7 @@ def findroot(f, x0, solver=Secant, tol=None, verbose=False, verify=True,
     If not abs(f(root)) < tol an exception is raised.
 
     Arguments:
+
     f : one dimensional function
     x0 : starting point, several starting points or interval (depends on solver)
     tol : the returned solution has an error smaller than this
@@ -518,6 +519,154 @@ def findroot(f, x0, solver=Secant, tol=None, verbose=False, verify=True,
     'secant', 'mnewton', 'halley', 'muller', 'illinois', 'pegasus', 'anderson',
     'ridder', 'anewton', 'bisect'
     See mpmath.optimization for their documentation.
+
+
+    Examples
+    ........
+
+    The function ``findroot`` locates a root of a given function using the
+    secant method by default. A simple example use of the secant method is to
+    compute pi as the root of sin(*x*) closest to *x* = 3:
+
+    >>> from mpmath import *
+    >>> mp.dps = 30
+    >>> print findroot(sin, 3)
+    3.14159265358979323846264338328
+
+    The secant method can be used to find complex roots of analytic functions,
+    although it must in that case generally be given a nonreal starting value
+    (or else it will never leave the real line):
+
+    >>> mp.dps = 15
+    >>> print findroot(lambda x: x**3 + 2*x + 1, j)
+    (0.226698825758202 + 1.46771150871022j)
+
+    A nice application is to compute nontrivial roots of the Riemann zeta
+    function with many digits (good initial values are needed for convergence):
+
+    >>> mp.dps = 30
+    >>> print findroot(zeta, 0.5+14j)
+    (0.5 + 14.1347251417346937904572519836j)
+
+    The secant method can also be used as an optimization algorithm, by passing
+    it a derivative of a function. The following example locates the positive
+    minimum of the gamma function:
+
+    >>> mp.dps = 20
+    >>> print findroot(lambda x: diff(gamma, x), 1)
+    1.4616321449683623413
+
+    Finally, a useful application is to compute inverse functions, such as the
+    Lambert W function which is the inverse of *w* exp(*w*), given the first
+    term of the solution's asymptotic expansion as the initial value. In basic
+    cases, this gives identical results to mpmath's builtin ``lambertw``
+    function:
+
+    >>> def lambert(x):
+    ...     return findroot(lambda w: w*exp(w) - x, log(1+x))
+    ...
+    >>> mp.dps = 15
+    >>> print lambert(1), lambertw(1)
+    0.567143290409784 0.567143290409784
+    >>> print lambert(1000), lambert(1000)
+    5.2496028524016 5.2496028524016
+
+
+    Multiple roots
+    --------------
+
+    For multiple roots all methods of the Newtonian family (including secant)
+    converge slowly. Consider this example:
+
+    >>> f = lambda x: (x - 1)**99
+    >>> findroot(f, 0.9, verify=False)
+    mpf('0.91807354244492868')
+
+    Even for a very close starting point the secant method converges very
+    slowly. Use ``verbose=True`` to illustrate this.
+
+    It's possible to modify Newton's method to make it converge regardless of
+    the root's multiplicity.
+
+    >>> findroot(f, -10, solver='mnewton')
+    mpf('1.0')
+
+    This variant uses the first and second derivative of the function, which is
+    not very efficient.
+
+    Alternatively you can use an experimental Newtonian solver that keeps track
+    of the speed of convergence and accelerates it using Steffensen's method if
+    necessary.
+
+    >>> findroot(f, -10, solver='anewton', verbose=True)
+    x: -9.88888888888888888889
+    error: 0.111111111111111111111
+    converging slowly
+    x: -9.77890011223344556678
+    error: 0.10998877665544332211
+    converging slowly
+    x: -9.67002233332199662166
+    error: 0.108877778911448945119
+    converging slowly
+    accelerating convergence
+    x: -9.5622443299551077669
+    error: 0.107778003366888854764
+    converging slowly
+    x: 0.99999999999999999214
+    error: 10.562244329955107759
+    x: 1.0
+    error: 7.8598304758094664213e-18
+    mpf('1.0')
+
+
+    Complex roots
+    -------------
+
+    For complex roots it's recommended to use Muller's method as it converges
+    even for real starting points very fast.
+
+    >>> findroot(lambda x: x**4 + x + 1, (0, 1, 2), solver='muller')
+    mpc(real='0.72713608449119684', imag='0.93409928946052944')
+
+
+    Intersection methods
+    --------------------
+
+    When you need to find a root in a known interval, it's highly recommended to
+    use an intersection-based solver like ``'anderson'`` or ``'ridder'``.
+    Usually the converge faster and more reliable. They have however problems
+    with multiple roots and usually need a sign change to find a root.
+
+    >>> findroot(lambda x: x**3, (-1, 1), solver='anderson')
+    mpf('0.0')
+
+    Be careful with symmetric functions:
+
+    >>> findroot(lambda x: x**2, (-1, 1), solver='anderson') #doctest:+ELLIPSIS
+    Traceback (most recent call last):
+      File "<stdin>", line 1, in <module>
+      File "mpmath/settings.py", line 135, in g
+        return f(*args, **kwargs)
+      File "mpmath/optimization.py", line 551, in findroot
+        for x, error in iterations:
+      File "mpmath/optimization.py", line 332, in __iter__
+        z = a - fa/s
+      File "<string>", line 8, in __div__
+      File ".../libmpf.py", line 761, in mpf_div
+        raise ZeroDivisionError
+    ZeroDivisionError
+
+    It fails even for better starting points, because there is no sign change:
+
+    >>> findroot(lambda x: x**2, (-1, .5), solver='anderson')
+    Traceback (most recent call last):
+      File "<stdin>", line 1, in <module>
+      File "mpmath/settings.py", line 135, in g
+        return f(*args, **kwargs)
+      File "mpmath/optimization.py", line 562, in findroot
+        % (abs(f(x)), tol))
+    ValueError: Could not find root within given tolerance. (1 > 2.1684e-19)
+    Try another starting point or tweak arguments.
     """
     # initialize arguments
     if not force_type:
@@ -564,9 +713,14 @@ def multiplicity(f, root, tol=eps, maxsteps=10, **kwargs):
     """
     Return the multiplicity of a given root of f.
 
-    Internally, numerical derivatives are used. This is very inefficient for
-    higher order derviatives. You can be specify the n-th derivative using the
-    dnf keyword.
+    Internally, numerical derivatives are used. This might be inefficient for
+    higher order derviatives. Due to this, ``multiplicity`` cancels after
+    evaluating 10 derivatives by default. You can be specify the n-th derivative
+    using the dnf keyword.
+
+    >>> from mpmath import *
+    >>> multiplicity(lambda x: sin(x) - 1, pi/2)
+    2
     """
     kwargs['d0f'] = f
     for i in xrange(maxsteps):
@@ -590,6 +744,66 @@ def steffensen(f):
 
     Definition:
     F(x) = (x*f(f(x)) - f(x)**2) / (f(f(x)) - 2*f(x) + x)
+
+    Example
+    .......
+
+    You can use Steffensen's method to accelerate a fixpoint iteration of linear
+    (or less) convergence.
+
+    x* is a fixpoint of the iteration x_{k+1} = phi(x_k) if x* = phi(x*). For
+    phi(x) = x**2 there are two fixpoints: 0 and 1. 
+
+    Let's try Steffensen's method:
+
+    >>> f = lambda x: x**2
+    >>> from mpmath.optimization import steffensen
+    >>> F = steffensen(f)
+    >>> for x in [0.5, 0.9, 2.0]:
+    ...     fx = Fx = x
+    ...     for i in xrange(10):
+    ...         try:
+    ...             fx = f(fx)
+    ...         except OverflowError:
+    ...             pass
+    ...         try:
+    ...             Fx = F(Fx)
+    ...         except ZeroDivisionError:
+    ...             pass
+    ...         print '%20g  %20g' % (fx, Fx)
+                    0.25                  -0.5
+                  0.0625                   0.1
+              0.00390625            -0.0011236
+            1.52588e-005          1.41691e-009
+            2.32831e-010         -2.84465e-027
+            5.42101e-020          2.30189e-080
+            2.93874e-039          -1.2197e-239
+            8.63617e-078                     0
+            7.45834e-155                     0
+            5.56268e-309                     0
+                    0.81               1.02676
+                  0.6561               1.00134
+                0.430467                     1
+                0.185302                     1
+               0.0343368                     1
+              0.00117902                     1
+            1.39008e-006                     1
+            1.93233e-012                     1
+            3.73392e-024                     1
+            1.39421e-047                     1
+                       4                   1.6
+                      16                1.2962
+                     256               1.10194
+                   65536               1.01659
+            4.29497e+009               1.00053
+            1.84467e+019                     1
+            3.40282e+038                     1
+            1.15792e+077                     1
+            1.34078e+154                     1
+            1.34078e+154                     1
+
+    Unmodified, the iteration converges only towards 0. Modified it converges
+    not only much faster, it converges even to the repelling fixpoint 1.
     """
     def F(x):
         fx = f(x)
