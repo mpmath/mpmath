24488ef8d5414aa7238072a0f2be4c16f8b6ccb6
Fredrik Johansson <fredrik.johansson@gmail.com>
2010-06-04 12:28:46 +0000

    document qfac and qgamma

--- mpmath/functions/qfunctions.py
+++ mpmath/functions/qfunctions.py
@@ -129,14 +129,74 @@ def qp(ctx, a, q=None, n=None, **kwargs):
 
 @defun_wrapped
 def qgamma(ctx, z, q, **kwargs):
+    r"""
+    Evaluates the q-gamma function
+
+    .. math ::
+
+        \Gamma_q(z) = \frac{(q; q)_{\infty}}{(q^z; q)_{\infty}} (1-q)^{1-z}.
+
+
+    **Examples**
+
+    Evaluation for real and complex arguments::
+
+        >>> from mpmath import *
+        >>> mp.dps = 25; mp.pretty = True
+        >>> qgamma(4,0.75)
+        4.046875
+        >>> qgamma(6,6)
+        121226245.0
+        >>> qgamma(3+4j, 0.5j)
+        (0.1663082382255199834630088 + 0.01952474576025952984418217j)
+
+    The q-gamma function satisfies a functional equation similar
+    to that of the ordinary gamma function::
+
+        >>> q = mpf(0.25)
+        >>> z = mpf(2.5)
+        >>> qgamma(z+1,q)
+        1.428277424823760954685912
+        >>> (1-q**z)/(1-q)*qgamma(z,q)
+        1.428277424823760954685912
+
+    """
+    if abs(q) > 1:
+        return ctx.qgamma(z,1/q)*q**((z-2)*(z-1)*0.5)
     return ctx.qp(q, q, None, **kwargs) / \
         ctx.qp(q**z, q, None, **kwargs) * (1-q)**(1-z)
 
 @defun_wrapped
 def qfac(ctx, z, q, **kwargs):
+    r"""
+    Evaluates the q-factorial,
+
+    .. math ::
+
+        [n]_q! = (1+q)(1+q+q^2)\cdots(1+q+\cdots+q^{n-1})
+
+    or more generally
+
+    .. math ::
+
+        [z]_q! = \frac{(q;q)_z}{(1-q)^z}.
+
+    **Examples**
+
+        >>> from mpmath import *
+        >>> mp.dps = 25; mp.pretty = True
+        >>> qfac(0,0)
+        1.0
+        >>> qfac(4,3)
+        2080.0
+        >>> qfac(5,6)
+        121226245.0
+        >>> qfac(1+1j, 2+1j)
+        (0.4370556551322672478613695 + 0.2609739839216039203708921j)
+
+    """
     if ctx.isint(z) and ctx._re(z) > 0:
         n = int(ctx._re(z))
-        print "yass", q, q, n
         return ctx.qp(q, q, n, **kwargs) / (1-q)**n
     return ctx.qgamma(z+1, q, **kwargs)
 
