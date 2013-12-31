4ce0b0d1db1942715b13ef4b50116bdb33f1fc4e
Fredrik Johansson <fredrik.johansson@gmail.com>
2013-12-31 19:41:53 +0100

    call this 0.18

--- pkgdocs.sh
+++ pkgdocs.sh
@@ -1,7 +1,7 @@
 TARFILE=mpmath-docsrc-`python -c "import mpmath; print mpmath.__version__"`.tar
 
-tar -cf $TARFILE README CHANGES pkgdocs.sh demo/*.py doc/*.py \
-  `find doc/source -not \( -name .svn -prune \) -type f | less`
+tar -cf $TARFILE README.rst CHANGES pkgdocs.sh demo/*.py doc/*.py \
+  `find doc/source -not \( -name .git -prune \) -type f | less`
 
 gzip $TARFILE
 
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(name='mpmath',
       description = 'Python library for arbitrary-precision floating-point arithmetic',
-      version='0.18-a1',
+      version='0.18',
       url='http://mpmath.googlecode.com',
       author='Fredrik Johansson',
       author_email='fredrik.johansson@gmail.com',
