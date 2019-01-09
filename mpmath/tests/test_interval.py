bce0e9afefdab2efb59b4af9548c75b0d2aaa23d
Jonathan Warner <warnerjon12@gmail.com>
2019-01-09 10:40:20 -0700

    Implemented float and complex conversions for ivmpf
    Response to issue fredrik-johansson#266

--- mpmath/tests/test_interval.py
+++ mpmath/tests/test_interval.py
@@ -439,3 +439,15 @@ def test_interval_gamma():
         assert z.b.ae(max_real)
         assert z.c.ae(min_imag)
         assert z.d.ae(max_imag)
+
+def test_interval_conversions():
+    mp.dps = 15
+    iv.dps = 15
+    for a, b in ((-0.0, 0), (0.0, 0.5), (1.0, 1), \
+                 ('-inf', 20.5), ('-inf', float(sqrt(2)))):
+        r = mpi(a, b)
+        assert int(r.b) == int(b)
+        assert float(r.a) == float(a)
+        assert float(r.b) == float(b)
+        assert complex(r.a) == complex(a)
+        assert complex(r.b) == complex(b)
