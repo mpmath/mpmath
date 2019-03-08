867d8784ce366bc2acde31cd4c1865bdde8fbf52
AlexV <asmodehn@gmail.com>
2019-03-08 13:56:09 +0100

    removing version retrieval via pkg_resources. static version string in code.

--- mpmath/__init__.py
+++ mpmath/__init__.py
@@ -1,9 +1,4 @@
-import pkg_resources
-try:
-    __version__ = pkg_resources.get_distribution(__name__).version
-except pkg_resources.DistributionNotFound:
-    __version__ = '1.1.0'
-del pkg_resources
+__version__ = '1.1.0'
 
 from .usertools import monitor, timing
 
