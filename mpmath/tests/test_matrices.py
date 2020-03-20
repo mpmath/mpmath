5abc70fc9bf289e3f0c2c3177d3cb77e3a5c01f3
Maximilian Gaukler <max.gaukler@fau.de>
2020-03-20 13:50:25 +0100

    Implement unary plus for matrices (+M == M rounded to current precision)

--- mpmath/tests/test_matrices.py
+++ mpmath/tests/test_matrices.py
@@ -166,9 +166,13 @@ def test_vector():
 def test_matrix_copy():
     A = ones(6)
     B = A.copy()
+    C = +A
     assert A == B
+    assert A == C
     B[0,0] = 0
     assert A != B
+    C[0,0] = 42
+    assert A != C
 
 def test_matrix_numpy():
     try:
