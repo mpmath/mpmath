1b890bc2ae2f6a476fd41cd0924ddb054930977a
Michael Dawson-Haggerty <mikedh@kerfed.com>
2021-03-18 13:57:09 -0400

    get version from __init__

--- setup.py
+++ setup.py
@@ -1,6 +1,17 @@
 #!/usr/bin/env python
 
+import os
 import setuptools
 
+# where version is stored relative to setup.py
+_version_path = os.path.join(
+    os.path.dirname(__file__),
+    'mpmath/__init__.py')
+# get version without importing anything
+# note that this is relying on __version__
+# being the *first* line of the __init__ file
+with open(_version_path, 'r') as f:
+    # use eval to get a clean string of version from file
+    __version__ = eval(f.readline().strip().split('=')[-1])
 
-setuptools.setup(use_scm_version=True)
+setuptools.setup(version=__version__)
