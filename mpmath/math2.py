064e9400bf57917977cdf687b2fc76b1afc11ae8
Sergey B Kirpichev <skirpichev@gmail.com>
2023-03-29 08:34:13 +0300

    Propagate nan in ei_asymp()
    Closes #480
    Closes #481
    Closes #482
    Closes #483
    Closes #484
    Closes #487

--- mpmath/math2.py
+++ mpmath/math2.py
@@ -502,14 +502,13 @@ EI_ASYMP_CONVERGENCE_RADIUS = 40.0
 
 def ei_asymp(z, _e1=False):
     r = 1./z
-    s = t = 1.0
     k = 1
-    while 1:
+    t = 1.0*k*r
+    s = 1.0 + t
+    while abs(t) >= 1e-16:
+        k += 1
         t *= k*r
         s += t
-        if abs(t) < 1e-16:
-            break
-        k += 1
     v = s*exp(z)/z
     if _e1:
         if type(z) is complex:
