385a3c86adcf124a55ae470d0c1ec9449ee7da8c
Sergey B Kirpichev <skirpichev@gmail.com>
2023-05-03 12:04:03 +0300

    Reorganize imports in mpmath/tests/test_summation.py (no star imports)

--- mpmath/tests/test_summation.py
+++ mpmath/tests/test_summation.py
@@ -1,4 +1,6 @@
-from mpmath import *
+from mpmath import (e, exp, fac, factorial, fp, fprod, fsum, inf, isnan, iv, j,
+                    log, mpi, nprod, nsum, pi, sumem)
+
 
 def test_sumem():
     assert sumem(lambda k: 1/k**2.5, [50, 100]).ae(0.0012524505324784962)
