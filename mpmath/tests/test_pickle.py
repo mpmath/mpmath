1acd3ef7c6e70d75b6f05d928639156a049a997c
Sergey B Kirpichev <skirpichev@gmail.com>
2024-02-07 07:16:49 +0300

    Support pickling for matrices
    Closes #380

--- mpmath/tests/test_pickle.py
+++ mpmath/tests/test_pickle.py
@@ -2,10 +2,11 @@ import pickle
 
 import pytest
 
-from mpmath import mpc, mpf
+from mpmath import matrix, mpc, mpf, sin
 
 
 @pytest.mark.parametrize('protocol', range(pickle.HIGHEST_PROTOCOL + 1))
-@pytest.mark.parametrize('obj', [mpf('0.5'), mpc('0.5','0.2')])
+@pytest.mark.parametrize('obj', [mpf('0.5'), mpc('0.5','0.2'),
+                                 matrix([1, sin(1)]), matrix([[1, 2], [3, 4]])])
 def test_pickle(obj, protocol):
     assert obj == pickle.loads(pickle.dumps(obj, protocol))
