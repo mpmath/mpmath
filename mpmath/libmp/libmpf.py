0d68ad52c940978a6800136a89378ed5b7a28067
Sergey B Kirpichev <skirpichev@gmail.com>
2024-04-04 07:17:51 +0300

    Adjust nan's hashing for Python versions >= 3.10

--- mpmath/libmp/libmpf.py
+++ mpmath/libmp/libmpf.py
@@ -442,7 +442,11 @@ def mpf_hash(s):
 
     # Handle special numbers
     if not sman:
-        if s == fnan: return sys.hash_info.nan
+        if s == fnan:
+            if sys.version_info >= (3, 10):
+                return object.__hash__(s)
+            else:
+                return sys.hash_info.nan
         if s == finf: return sys.hash_info.inf
         if s == fninf: return -sys.hash_info.inf
 
