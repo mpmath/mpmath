dcc591cb678dcef0c0b37f45d430d105ee258766
Sergey B Kirpichev <skirpichev@gmail.com>
2024-04-14 08:38:51 +0300

    Fallback to fixed precision if prec change is infinite
    This will leave handling of special numbers to fewer
    functions, e.g. to hypercomb().
    
    Closes #520
    Closes #522

--- mpmath/functions/hypergeometric.py
+++ mpmath/functions/hypergeometric.py
@@ -316,10 +316,8 @@ def _hyp1f1(ctx, a_s, b_s, z, **kwargs):
         return ctx.one+z
     magz = ctx.mag(z)
     if magz >= 7 and not (ctx.isint(a) and ctx.re(a) <= 0):
-        if ctx.isinf(z):
-            if ctx.sign(a) == ctx.sign(b) == ctx.sign(z) == 1:
-                return ctx.inf
-            return ctx.nan * z
+        if ctx.isinf(z) and ctx.sign(a) == ctx.sign(b) == ctx.sign(z) == 1:
+            return ctx.inf
         try:
             try:
                 ctx.prec += magz
