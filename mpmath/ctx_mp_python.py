e33a9aba2233f0f67f9330ce66e93f5a1524943d
Sergey B Kirpichev <skirpichev@gmail.com>
2024-04-24 14:28:54 +0300

    Drop mpf.__complex__()
    Closes #794

--- mpmath/ctx_mp_python.py
+++ mpmath/ctx_mp_python.py
@@ -140,7 +140,6 @@ class _mpf(mpnumeric):
     def __hash__(s): return mpf_hash(s._mpf_)
     def __int__(s): return int(to_int(s._mpf_))
     def __float__(s): return to_float(s._mpf_, rnd=s.context._prec_rounding[1])
-    def __complex__(s): return complex(float(s))
     def __bool__(s): return s._mpf_ != fzero
 
     def __abs__(s):
