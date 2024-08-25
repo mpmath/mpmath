a1dd8d987920a663973dfe8d1cad820a6bac2eaa
javierelpianista <javier.garcia.tw@hotmail.com>
2024-08-25 11:36:28 -0300

    Remove test that causes timeout

--- mpmath/tests/test_convert.py
+++ mpmath/tests/test_convert.py
@@ -77,8 +77,6 @@ def test_to_str():
     assert to_str(from_str('1', base=16), 6, base=16, binary_exp=True) == '0x1.0'
     pytest.raises(ValueError, lambda: to_str(from_str('1', base=16),
                                              6, binary_exp=True))
-    with pytest.raises(ValueError):
-        to_str(from_str('1.99999999', prec=0), 6)
 
 def test_pretty():
     mp.pretty = True
