9f9a1ae39bb8f53de378c3ffd46a080628b5eb8e
Riccardo Orsi <104301293+ricor07@users.noreply.github.com>
2025-01-18 16:20:05 +0100

    Update zeta.py
    Tries to address error in polylog precision when abs(z) > 1 and s is close to an integer

--- mpmath/functions/zeta.py
+++ mpmath/functions/zeta.py
@@ -482,7 +482,7 @@ def polylog(ctx, s, z):
         return z/(1-z)**2
     if abs(z) <= 0.75 or (not ctx.isint(s) and abs(z) < 0.9):
         return polylog_series(ctx, s, z)
-    if abs(z) >= 1.4 and ctx.isint(s):
+    if abs(z) >= 1.4 and ctx.isint(s) or abs(s - int(s)) < 10e-6:
         return (-1)**(s+1)*polylog_series(ctx, s, 1/z) + polylog_continuation(ctx, int(ctx.re(s)), z)
     if ctx.isint(s):
         return polylog_unitcircle(ctx, int(ctx.re(s)), z)
