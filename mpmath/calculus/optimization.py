58859b9acf9ac6ce080a2aed06254131339c5fc5
Sergey B Kirpichev <skirpichev@gmail.com>
2025-05-30 06:04:23 +0300

    Fix Anderson docs

--- mpmath/calculus/optimization.py
+++ mpmath/calculus/optimization.py
@@ -446,7 +446,7 @@ def Anderson(*args, **kwargs):
     1d-solver generating pairs of approximative root and error.
 
     Uses Anderson-Bjoerk method to find a root of f in [a, b].
-    Wrapper for illinois to use method='pegasus'.
+    Wrapper for illinois to use method='anderson'.
     """
     kwargs['method'] = 'anderson'
     return Illinois(*args, **kwargs)
