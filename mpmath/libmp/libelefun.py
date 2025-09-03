baaccb59aa9ba80d744d842a815c8e36648310f3
hnb22 <haden.borjeson@yahoo.com>
2025-09-02 19:31:22 -0700

    Switch logarithm implementation to taylor series when input is close to 1 (#957)
    This modifies the logarithm implementation for cases where taylor series
    is more optimal (cancellation >= working precision), providing a faster
    path for low precision cases depending on how close input is to 1.

--- mpmath/libmp/libelefun.py
+++ mpmath/libmp/libelefun.py
@@ -699,9 +699,18 @@ def mpf_ln(x, prec, rnd=round_fast):
             return mpf_perturb(t, tsign, prec, rnd)
         else:
             wp += cancellation
-        # TODO: if close enough to 1, we could use Taylor series
+
+        # If close enough to 1, use Taylor series
         # even in the AGM precision range, since the Taylor series
-        # converges rapidly
+        # converges rapidly.
+        # Taylor = AGM when O~(prec) = O~(prec^2/cancellation) where cancellation
+        # is greater than or equal to precision
+        wpb = wp.bit_length()
+        if wpb <= cancellation:  # possibly include constant (big integer operations)
+            a = to_fixed(x, wp)
+            s = log_taylor(a, wp)
+            return from_man_exp(s, -wp, prec, rnd)
+
     #------------------------------------------------------------------
     # Another special case:
     # n*log(2) is a good enough approximation
