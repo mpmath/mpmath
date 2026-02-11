80972019f4e2bad0aeafcbbb98c4e5deb7fac410
Sergey B Kirpichev <skirpichev@gmail.com>
2026-02-12 01:15:56 +0300

    Apply suggestion from @skirpichev

--- mpmath/tests/test_linalg.py
+++ mpmath/tests/test_linalg.py
@@ -4,7 +4,7 @@ import pytest
 
 from mpmath import (cond, det, diag, exp, expm, extend, extradps, eye, fp,
                     hilbert, inf, inverse, iv, j, lu, lu_solve, matrix, mnorm,
-                    mp, mpc, mpf, nint, norm, pi, pinv, qr, qr_solve, rand, rank,
+                    mp, mpc, mpf, nint, norm, pi, pinv, qr, qr_solve, rand, rank, svd,
                     randmatrix, residual, zeros, absmin, eps)
 
 
