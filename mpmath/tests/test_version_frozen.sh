1ad636991c62cca4349681deb85746b3dd5ed76a
Flurin Arner <arner@speag.swiss>
2026-03-06 08:21:30 +0100

    Manual improvements from PR suggestions
    - Working directory independent root directory for mpmath
    - Remove *.spec from initial clean up (legacy from pyinstaller before mktemp usage)
    - Update echo and remove redundant comments

--- mpmath/tests/test_version_frozen.sh
+++ mpmath/tests/test_version_frozen.sh
@@ -2,14 +2,11 @@
 # Test that the version number is provided correctly in a frozen (bundled) executable (see #1044).
 set -e
 
-# Clean up any previous test artifacts
 echo "Cleaning up previous test artifacts..."
-rm -rf build dist *.spec .venv
+rm -rf build dist .venv
 
 # Get the directory of the current repository
-MPMATH_DIR=$(pwd)
-# # Working directory independent version: 
-# MPMATH_DIR=$(realpath "$(dirname "$(realpath "$0")")/../..")
+MPMATH_DIR=$(realpath "$(dirname "$(realpath "$0")")/../..")
 echo "Repo mpmath directory: $MPMATH_DIR"
 TEMP_DIR=$(mktemp -d)
 echo "Created temporary directory: $TEMP_DIR"
@@ -19,10 +16,8 @@ echo "Creating virtual environment..."
 python3 -m venv .venv
 source .venv/bin/activate
 
-
 cd "$MPMATH_DIR"
-echo "Building tarball from local repo..."
-# Build the source distribution
+echo "Building the source distribution from the local repo..."
 python3 -m pip install build pyinstaller
 python3 -m build --sdist
 
@@ -33,7 +28,6 @@ echo "Generated tarball: $TARBALL"
 echo "Installing mpmath from tarball..."
 pip install dist/"$(basename $TARBALL)"
 cd "$TEMP_DIR" # Return to temporary folder to run the version script
-
 
 # Create version_script.py that imports mpmath and prints the package version
 cat << EOF > version_script.py
@@ -49,9 +43,6 @@ pyinstaller --onefile --clean version_script.py
 
 echo "Run frozen executable and extract the version from the output"
 FROZEN_VERSION=$(./dist/version_script 2>&1)
-
-# Return to the original repository directory
-cd "$MPMATH_DIR" 
 deactivate
 
 if [ "$DIRECT_VERSION" == "$FROZEN_VERSION" ]; then
