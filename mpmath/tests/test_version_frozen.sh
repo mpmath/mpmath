54f2aa705fb06f3d2919017634614b3a03e5d3ec
Sergey B Kirpichev <skirpichev@gmail.com>
2026-03-07 04:13:02 +0300

    Some cleanup for script

--- mpmath/tests/test_version_frozen.sh
+++ mpmath/tests/test_version_frozen.sh
@@ -1,24 +1,18 @@
 #!/bin/bash
-# Test that the version number is provided correctly in a frozen (bundled) executable (see #1044).
+#
+# Test that the version number is provided correctly in a frozen (bundled)
+# executable (see #1044).
+
 set -e
 
-echo "Cleaning up previous test artifacts..."
-rm -rf build dist .venv
-
 # Get the directory of the current repository
-MPMATH_DIR=$(realpath "$(dirname "$(realpath "$0")")/../..")
+MPMATH_DIR="$(realpath "$(dirname "$0")/../../")"
 echo "Repo mpmath directory: $MPMATH_DIR"
-TEMP_DIR=$(mktemp -d)
-echo "Created temporary directory: $TEMP_DIR"
-cd "$TEMP_DIR"
 
-echo "Creating virtual environment..."
-python3 -m venv .venv
-source .venv/bin/activate
-
-cd "$MPMATH_DIR"
-echo "Building the source distribution from the local repo..."
+echo "Install requirements..."
 python3 -m pip install build pyinstaller
+
+echo "Building the source distribution from the local repo..."
 python3 -m build --sdist
 
 # Find and install the generated tarball
@@ -27,23 +21,25 @@ echo "Generated tarball: $TARBALL"
 
 echo "Installing mpmath from tarball..."
 pip install dist/"$(basename $TARBALL)"
-cd "$TEMP_DIR" # Return to temporary folder to run the version script
 
-# Create version_script.py that imports mpmath and prints the package version
+TEMP_DIR=$(mktemp -d)
+echo "Created temporary directory: $TEMP_DIR"
+cd "$TEMP_DIR"
+
+# Create version_script.py that prints the package version
 cat << EOF > version_script.py
 import mpmath
 print(mpmath.__version__)
 EOF
 
 # Save local version for later comparison
-DIRECT_VERSION=$(python -m mpmath --version)
+DIRECT_VERSION="$(python3 -m mpmath --version)"
 
 echo "Building version_script with PyInstaller..."
 pyinstaller --onefile --clean version_script.py
 
-echo "Run frozen executable and extract the version from the output"
-FROZEN_VERSION=$(./dist/version_script 2>&1)
-deactivate
+echo "Run frozen executable and extract the version from the output..."
+FROZEN_VERSION="$(./dist/version_script 2>&1)"
 
 if [ "$DIRECT_VERSION" == "$FROZEN_VERSION" ]; then
     echo "Test passed: Version matches in frozen (bundled) executable."
