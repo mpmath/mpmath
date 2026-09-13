f105d268b7a419fdc5e78794e190e776fcfd26d1
Matt Haberland <matt.haberland@gmail.com>
2026-09-13 08:01:10 -0700

    Address log special-case review comments

--- mpmath/libmp/libelefun.py
+++ mpmath/libmp/libelefun.py
@@ -766,8 +766,7 @@ def mpf_log_hypot(a, b, prec, rnd):
             if a == b == fzero:
                 return fninf
             if a in (finf, fninf) or b in (finf, fninf):
-                # at least one term is (+/- inf)^2
-                return finf
+                return finf  # at least one term is (+/- inf)^2
             return fnan
         # only a is inf/nan/0
         if a == fzero:
@@ -897,9 +896,7 @@ def mpf_atan2(y, x, prec, rnd=round_down):
             if mpf_sign(x) >= 0:
                 return fzero
             return mpf_pi(prec, rnd)
-        if y in (finf, fninf):
-            if x == fnan:
-                return fnan
+        if y in (finf, fninf) and x != fnan:
             if x == finf:
                 if y == finf:
                     return mpf_shift(mpf_pi(prec, rnd), -2)
